20295fde3bcab5f97419c96ed37a9036d6c0800f
Michael Andres <ma@suse.de>
2012-12-12 10:59:34 +0100

    HACK: interim continue using deprecated history reader API

--- PROJECTINFO.cmake
+++ PROJECTINFO.cmake
@@ -15,3 +15,20 @@ SET( URL                "http://github.com/libyui/" )
 SET( SUMMARY            "Libyui - Gtk Package Selector" )
 SET( DESCRIPTION        "This package contains the Gtk package selector\ncomponent for libYUI.\n" )
 
+# libzypp-12.5.0 changes:
+# - HistoryLogReader: new HistoryLogData based API for parsing the new
+#   history file entries (fate#312521)
+#   The old HistoryItem based API is deprecated but will
+#   still be available for a while if you compile with
+#   -DWITH_DEPRECATED_HISTORYITEM_API.
+#
+#  roughly:
+#  ///   - // old style
+#  ///   - HistoryItem::Ptr ptr;
+#  ///   - Date d = ptr->date;
+#  ///   + // new style
+#  ///   + HistoryLogData::Ptr ptr;
+#  ///   + Date d = ptr->date();
+#
+#  http://doc.opensuse.org/projects/libzypp/HEAD/classzypp_1_1HistoryLog.html
+ADD_DEFINITIONS(-DWITH_DEPRECATED_HISTORYITEM_API=1)
